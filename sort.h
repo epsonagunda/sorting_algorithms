2bb52d666f7917d9aab3239822731620c4f13ceb
epsonagunda <epsonagunda182@gmail.com>
2024-04-29 00:22:33 -0700

    Sorting algorithms

--- sort.h
+++ sort.h
@@ -25,5 +25,11 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+listint_t *create_listint(const int *array, size_t size);
+int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size);
+void qs(int *array, ssize_t first, ssize_t last, int size);
+void quick_sort(int *array, size_t size);
+
+
 
 #endif
